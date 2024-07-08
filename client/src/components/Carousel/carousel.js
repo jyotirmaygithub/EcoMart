2a61fa263ab78f9d00bb408eee9e21c0f822b467
jyotirmay <jyotirmay2000gupta@gmail.com>
2024-07-08 18:58:47 +0530

    Feat : carousel

--- client/src/components/Carousel/carousel.js
+++ client/src/components/Carousel/carousel.js
@@ -1,9 +1,43 @@
-import React from 'react'
+import AwesomeSlider from "react-awesome-slider";
+import "react-awesome-slider/dist/styles.css";
 
-export default function carousel() {
+export default function Slider() {
   return (
-    <div>
-      
+    <div className="slider-wrapper">
+      <AwesomeSlider
+        className="slider"
+        bullets={false} // Disable navigation bullets
+        style={{ height: "500px" }} // Adjust slider height as needed
+      >
+        <div>
+          <img
+            src="https://images.unsplash.com/photo-1581655353564-df123a1eb820?crop=entropy&cs=srgb&fm=jpg&ixid=M3w1MTIzNDJ8MHwxfGFsbHx8fHx8fHx8fDE3MjA0NDQ1ODF8&ixlib=rb-4.0.3&q=85"
+            alt="cloth 1"
+            className="slider-image"
+          />
+        </div>
+        <div>
+          <img
+            src="https://images.unsplash.com/photo-1560769629-975ec94e6a86?crop=entropy&cs=srgb&fm=jpg&ixid=M3w1MTIzNDJ8MHwxfGFsbHx8fHx8fHx8fDE3MjA0NDUxMTN8&ixlib=rb-4.0.3&q=85"
+            alt="cloth 2"
+            className="slider-image"
+          />
+        </div>
+        <div>
+          <img
+            src="https://images.unsplash.com/photo-1581655353564-df123a1eb820?crop=entropy&cs=srgb&fm=jpg&ixid=M3w1MTIzNDJ8MHwxfGFsbHx8fHx8fHx8fDE3MjA0NDQ1ODF8&ixlib=rb-4.0.3&q=85"
+            alt="cloth 3"
+            className="slider-image"
+          />
+        </div>
+        <div>
+          <img
+            src="https://images.unsplash.com/photo-1581655353564-df123a1eb820?crop=entropy&cs=srgb&fm=jpg&ixid=M3w1MTIzNDJ8MHwxfGFsbHx8fHx8fHx8fDE3MjA0NDQ1ODF8&ixlib=rb-4.0.3&q=85"
+            alt="cloth 4"
+            className="slider-image"
+          />
+        </div>
+      </AwesomeSlider>
     </div>
-  )
+  );
 }
