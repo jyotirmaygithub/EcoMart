dc3b717ca8f63b77db7cf0ba57ab173a0f6ae54f
jyotirmay <jyotirmay2000gupta@gmail.com>
2024-07-08 14:46:18 +0530

    update : store update with the cart reducer

--- client/src/store.js
+++ client/src/store.js
@@ -1,14 +1,20 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
-import {thunk} from "redux-thunk";
+import { thunk } from "redux-thunk";
+import { userReducer } from "./reducer/userReducer";
 import {
-  userReducer,
-} from "./reducer/userReducer";
-import {productsReducer} from "./reducer/productReducer"
+  productsReducer,
+  productDetailsReducer,
+} from "./reducer/productReducer";
+import {authReducer} from "./reducer/authReducer"
+import {cartReducer} from "./reducer/cartReducer"
 
 // Combine your reducers into a root reducer
 const rootReducer = combineReducers({
   userData: userReducer,
   products: productsReducer,
+  productDetails: productDetailsReducer,
+  auth : authReducer,
+  cart : cartReducer
 });
 
 // Create an array of middleware (in this case, just redux-thunk)
