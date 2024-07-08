eb24ad9d51773bd8f036eb2e061da9e5eae69392
jyotirmay <jyotirmay2000gupta@gmail.com>
2024-07-08 14:47:11 +0530

    Feat : frontend of the cart

---
+++ client/src/components/Cart/cart.js
@@ -0,0 +1,252 @@
+import React, { useState , useEffect} from "react";
+import "./cart.css";
+import TextField from "@mui/material/TextField";
+import { useSelector, useDispatch } from "react-redux";
+import { retrieveCartItems, removeItemFromCart } from "../../actions/cartAction";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import RemoveShoppingCartIcon from "@mui/icons-material/RemoveShoppingCart";
+import { Link } from "react-router-dom";
+// import MetaData from "../layouts/MataData/MataData";
+import { useNavigate } from "react-router-dom";
+import CartItem from "./cartItem";
+import { dispalyMoney, generateDiscountedPrice } from "../DisplayMoney/displayMoney";
+
+const Cart = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const { cartItems } = useSelector((state) => state.cart);
+  
+  useEffect(()=>{
+    dispatch(retrieveCartItems());
+  },[dispatch])
+
+  // New code
+  const [couponCode, setCouponCode] = useState("");
+  const [isFocused, setIsFocused] = useState(false);
+  const [isValid, setIsValid] = useState(true);
+
+  // New code end
+
+  const increaseQuantity = (id, quantity, stock) => {
+    const newQty = quantity + 1;
+    if (stock <= quantity) {
+      return;
+    } else {
+    //   dispatch(addItemToCart(id, newQty));
+    }
+  };
+
+  const decreaseQuantity = (id, quantity) => {
+    const newQty = quantity - 1;
+    if (1 >= quantity) {
+      return;
+    }
+
+    // dispatch(addItemToCart(id, newQty));
+  };
+
+  // New code
+  const handleApplyCoupon = () => {
+    // Handle apply coupon logic
+    setIsValid(false);
+  };
+
+  const handleFocus = (event) => {
+    setIsFocused(event.target.value !== "");
+  };
+
+  // New code end
+
+  const deleteCartItems = (id) => {
+    console.log("id = ",id)
+    // dispatch(removeItemFromCart(id));
+  };
+
+  const checkoutHandler = () => {
+    navigate("/login?redirect=/shipping");
+  };
+
+  // Calculate price after discount
+  let totalPrice = cartItems.reduce(
+    (acc, item) => acc + item.price * item.quantity,
+    0
+  );
+  let discountedPrice = generateDiscountedPrice(totalPrice);
+  let totalDiscount = totalPrice - discountedPrice;
+  let final = totalPrice - totalDiscount;
+  final = dispalyMoney(final);
+  totalDiscount = dispalyMoney(totalDiscount);
+  totalPrice = dispalyMoney(totalPrice);
+
+  return (
+    <>
+      <div className="cartPage">
+        {/* <MetaData title="Your Cart" /> */}
+        <div className="cart_HeaderTop">
+          <div className="headerLeft">
+            <Typography variant="h5" component="h1" className="cartHeading">
+              Shopping Cart
+            </Typography>
+            <Typography variant="body2" className="cartText3">
+              {/* TOTAL ({cartItems.length} item) <b>{final}</b> */}
+            </Typography>
+          </div>
+          <Typography
+            variant="body2"
+            className="cartText2"
+            onClick={() => navigate("/products")}
+          >
+            Continue Shopping
+          </Typography>
+        </div>
+
+        <div className="separator_cart2"></div>
+
+        {cartItems.length === 0 ? (
+          <div className="emptyCartContainer">
+            <RemoveShoppingCartIcon className="cartIcon" />
+
+            <Typography variant="h5" component="h1" className="cartHeading">
+              Your Shopping Cart is Empty
+            </Typography>
+            <Typography variant="body2" className="cartText">
+              Nothin' to see here.
+            </Typography>
+            <Typography variant="body2" className="cartText">
+              Let's get shopping!
+            </Typography>
+            <Link to="/products">
+              <Button className="shopNowButton">Shop Now</Button>
+            </Link>
+          </div>
+        ) : (
+          <>
+            <div className="cart_content_wrapper">
+              <div className="cart_left_container">
+                {cartItems &&
+                  cartItems.map((item) => (
+                    <Link
+                      to="#"
+                      style={{ textDecoration: "none", color: "none" }}
+                      key={item.productId}
+                    >
+                      <CartItem
+                        item={item}
+                        deleteCartItems={deleteCartItems}
+                        decreaseQuantity={decreaseQuantity}
+                        increaseQuantity={increaseQuantity}
+                        length={cartItems.length}
+                        id={item.productId}
+                      />
+                    </Link>
+                  ))}
+              </div>
+
+              <div className="separator_cart3"></div>
+              <div className="cart_right_container">
+                <div className="order_summary">
+                  <h4>
+                    Order Summary &nbsp; ( {cartItems.length}{" "}
+                    {cartItems.length > 1 ? "items" : "item"} )
+                  </h4>
+                  <div className="order_summary_details">
+                    <div className="price order_Summary_Item">
+                      <span>Original Price</span>
+                      {/* ORIGINAL PRICE TOTAL */}
+                      <p>{totalPrice}</p>
+                    </div>
+
+                    <div className="discount order_Summary_Item">
+                      <span>Discount</span>
+                      <p>
+                        <del>{totalDiscount}</del>
+                      </p>
+                    </div>
+
+                    <div className="delivery order_Summary_Item">
+                      <span>Delivery</span>
+                      <p>
+                        <b>Free</b>
+                      </p>
+                    </div>
+
+                    <div className="separator_cart"></div>
+                    <div className="total_price order_Summary_Item">
+                      <div>
+                        <h4>Total Price</h4>
+
+                        <p
+                          style={{
+                            fontSize: "14px",
+                            marginTop: "-10px",
+                            color: "#414141",
+                          }}
+                        >
+                          (Inclusive of all taxes)
+                        </p>
+                      </div>
+                      <p>
+                        <b>{final}</b>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="separator"></div>
+
+                <div className="coupon-box-wrapper">
+                  <div
+                    className={`coupon-box-content ${
+                      isFocused ? "focused" : ""
+                    }`}
+                  >
+                    <TextField
+                      label="Enter coupon code"
+                      value={couponCode}
+                      onChange={(e) => setCouponCode(e.target.value)}
+                      onFocus={handleFocus}
+                      onBlur={() => setIsFocused(false)}
+                      error={!isValid}
+                      helperText={!isValid && "Invalid coupon code"}
+                      variant="outlined"
+                      size="small"
+                      style={{ width: "200px", marginRight: "1rem" }}
+                    />
+                    <Button
+                      variant="contained"
+                      color="primary"
+                      className="coupon-box-apply-btn"
+                      onClick={handleApplyCoupon}
+                    >
+                      Apply
+                    </Button>
+                  </div>
+                </div>
+
+                <Button
+                  variant="contained"
+                  className="btn-custom"
+                  onClick={checkoutHandler}
+                >
+                  Checkout
+                </Button>
+
+                <div className="paymentLogoImg">
+                  <img
+                    src={""}
+                    alt="payment-icons"
+                    className="paymentImg"
+                  />
+                </div>
+              </div>
+            </div>
+          </>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default Cart;
---
+++ client/src/components/Cart/cartItem.js
@@ -0,0 +1,265 @@
+import React from "react";
+import { styled } from "@mui/system";
+import {
+  Card,
+  CardMedia,
+  CardContent,
+  Typography,
+  IconButton,
+  Input,
+} from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
+import RemoveIcon from "@mui/icons-material/Remove";
+import AddIcon from "@mui/icons-material/Add";
+import {
+  dispalyMoney,
+  generateDiscountedPrice,
+} from "../DisplayMoney/displayMoney";
+
+const StyledCard = styled(Card)(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  padding: "1.5rem 2rem",
+  width: "fit-content",
+  boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.2)",
+  margin: "1rem 2rem",
+  height: "auto",
+  [theme.breakpoints.down(899)]: {
+    padding: "3rem 3rem",
+    margin: "1rem 3rem",
+  },
+  [theme.breakpoints.down(699)]: {
+    padding: "2rem",
+    margin: "1rem",
+    width: "80%",
+  },
+  [theme.breakpoints.down(499)]: {
+    padding: "2rem",
+    margin: "1rem",
+    width: "65%",
+  },
+}));
+
+const StyledCardMedia = styled(CardMedia)(({ theme }) => ({
+  width: "200px",
+  height: "240px",
+  marginRight: "16px",
+  [theme.breakpoints.down(699)]: {
+    width: "35%",
+    marginLeft: "-2rem",
+    paddingRight: "1rem",
+  },
+  [theme.breakpoints.down(599)]: {
+    width: "30%",
+    marginLeft: "-2rem",
+    paddingRight: "1rem",
+  },
+  [theme.breakpoints.down(499)]: {
+    width: "20%",
+    marginLeft: "-2rem",
+    paddingRight: "1rem",
+  },
+}));
+
+const StyledCardContent = styled(CardContent)(({ theme }) => ({
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "space-between",
+  width: "fit-content",
+  [theme.breakpoints.down(699)]: {
+    padding: "0",
+    width: "fit-content",
+  },
+  [theme.breakpoints.down(599)]: {
+    padding: "0",
+    width: "fit-content",
+  },
+}));
+
+const CartHeader = styled("div")({
+  display: "flex",
+  justifyContent: "space-around",
+  alignItems: "flex-start",
+});
+
+const TitleTypography = styled(Typography)(({ theme }) => ({
+  width: "90%",
+  fontSize: "1rem",
+  fontWeight: 600,
+  marginLeft: "1rem",
+  [theme.breakpoints.down(599)]: {
+    fontSize: "14px",
+    marginLeft: "0",
+  },
+  "& .MuiTypography-subtitle1": {
+    [theme.breakpoints.down(599)]: {
+      fontSize: "14px",
+    },
+  },
+}));
+
+const CartDeleteIconButton = styled(IconButton)(({ theme }) => ({
+  color: "black",
+  marginTop: "-.5rem",
+  [theme.breakpoints.down(599)]: {
+    marginRight: "-2.5rem",
+  },
+  "&:hover": {
+    color: "#ed1c24",
+  },
+  [theme.breakpoints.down(499)]: {
+    marginRight: "-2rem",
+  },
+}));
+
+const PriceItem = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "baseline",
+  gap: "1rem",
+  marginLeft: "1.2rem",
+  [theme.breakpoints.down(599)]: {
+    marginLeft: "0rem",
+    marginRight: "-1rem",
+  },
+}));
+
+const CartSubHeadingsTypography = styled(Typography)(({ theme }) => ({
+  fontSize: "16px",
+  fontWeight: 500,
+  textTransform: "uppercase",
+  color: "#414141",
+  [theme.breakpoints.down(599)]: {
+    fontSize: "14px",
+  },
+  [theme.breakpoints.down(499)]: {
+    fontSize: "12px",
+  },
+}));
+
+const ItemPriceTypography = styled(Typography)(({ theme }) => ({
+  fontSize: "16px",
+  fontWeight: 400,
+  [theme.breakpoints.down(599)]: {
+    fontSize: "14px",
+  },
+  [theme.breakpoints.down(499)]: {
+    fontSize: "13px",
+  },
+}));
+
+const ItemOldPriceTypography = styled(Typography)(({ theme }) => ({
+  marginLeft: "-8px",
+  fontSize: "14px",
+  fontWeight: 400,
+  [theme.breakpoints.down(499)]: {
+    fontSize: "12px",
+  },
+}));
+
+const ContentBottom = styled("div")(({ theme }) => ({
+  display: "flex",
+  justifyContent: "space-around",
+  marginTop: "1rem",
+  alignItems: "baseline",
+  width: "fit-content",
+  flexDirection: "column",
+  [theme.breakpoints.down(599)]: {
+    marginLeft: "0rem",
+    marginRight: "-1rem",
+  },
+  [theme.breakpoints.down(550)]: {
+    position: "relative",
+    marginLeft: "0rem",
+  },
+}));
+
+function CartItem({
+  deleteCartItems,
+  item,
+  decreaseQuantity,
+  increaseQuantity,
+  length,
+}) {
+  // Calculate price after discount
+  let finalPrice = generateDiscountedPrice(item.price);
+  let discountedPrice = item.price - finalPrice;
+  discountedPrice = dispalyMoney(discountedPrice);
+  let total = finalPrice * item.quantity;
+  total = dispalyMoney(total);
+  finalPrice = dispalyMoney(finalPrice);
+  return (
+    <StyledCard>
+      <StyledCardMedia
+        image={item.image}
+        title={item.name}
+      />
+      <StyledCardContent>
+        <div>
+          <CartHeader>
+            <TitleTypography variant="subtitle1">
+              {item.name}
+            </TitleTypography>
+            <CartDeleteIconButton
+              aria-label="delete"
+              onClick={() => deleteCartItems(item.productId)}
+            >
+              <DeleteIcon />
+            </CartDeleteIconButton>
+          </CartHeader>
+          <PriceItem>
+            <CartSubHeadingsTypography variant="body2">
+              Price:
+            </CartSubHeadingsTypography>
+            <ItemPriceTypography variant="subtitle1">
+              {finalPrice}
+            </ItemPriceTypography>
+            <ItemOldPriceTypography
+              variant="caption"
+              component="span"
+              color="black"
+            >
+              <del>{discountedPrice}</del>
+            </ItemOldPriceTypography>
+          </PriceItem>
+        </div>
+        <ContentBottom>
+          <div className="prod_details_additem">
+            <h5>QTY:</h5>
+            <div className="additem">
+              <IconButton
+                onClick={() => decreaseQuantity(item.productId, item.quantity)}
+                className="additem_decrease"
+              >
+                <RemoveIcon />
+              </IconButton>
+              <Input
+                readOnly
+                type="number"
+                value={item.quantity}
+                className="input"
+              />
+              <IconButton
+                onClick={() =>
+                  increaseQuantity(item.productId, item.quantity, item.stock)
+                }
+                className="additem_increase"
+              >
+                <AddIcon />
+              </IconButton>
+            </div>
+          </div>
+          <PriceItem>
+            <CartSubHeadingsTypography variant="body2">
+              TOTAL:
+            </CartSubHeadingsTypography>
+            <Typography variant="subtitle1">
+              {total}
+            </Typography>
+          </PriceItem>
+        </ContentBottom>
+      </StyledCardContent>
+    </StyledCard>
+  );
+}
+
+export default CartItem;
