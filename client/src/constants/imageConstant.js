699667306fbf0489258376053fb2b0b7d3c61750
jyotirmay <jyotirmay2000gupta@gmail.com>
2024-07-12 04:46:50 +0530

    update : constants

---
+++ client/src/constants/imageConstant.js
@@ -0,0 +1,5 @@
+// src/constants/imageConstants.js
+
+export const IMAGE_UPLOAD_REQUEST = 'IMAGE_UPLOAD_REQUEST';
+export const IMAGE_UPLOAD_SUCCESS = 'IMAGE_UPLOAD_SUCCESS';
+export const IMAGE_UPLOAD_FAIL = 'IMAGE_UPLOAD_FAIL';
