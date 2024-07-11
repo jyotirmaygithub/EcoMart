ff7a838c438ebf454884c66bd69af64f74ab972e
jyotirmay <jyotirmay2000gupta@gmail.com>
2024-07-12 04:48:33 +0530

    Feat : loaders

--- client/src/layout/loader/Loader.js
+++ client/src/layout/loader/Loader.js
@@ -1,6 +1,6 @@
 import * as React from "react";
 import CircularProgress from "@mui/material/CircularProgress";
 
-export default function CircularIndeterminate() {
-  return <CircularProgress size={23} sx={{ color: "white" }} />;
+export default function CircularIndeterminate({size, color}) {
+  return <CircularProgress size={size} sx={{ color: color}} />;
 }
--- client/src/layout/loader/gitLoader.js
+++ client/src/layout/loader/gitLoader.js
@@ -3,8 +3,8 @@ import Loading from "../../assets/shopping-loader.gif"; // Correct import
 
 export default function Loader() {
   return (
-    <div className="fixed inset-0  bg-white bg-opacity-80 flex justify-center items-center z-50">
-      <img src={Loading} alt="Loading..." className="w-[60%]" />
+    <div className="flex justify-center items-center w-full mt-28">
+      <img src={Loading} alt="Loading..." />
     </div>
   );
 }
